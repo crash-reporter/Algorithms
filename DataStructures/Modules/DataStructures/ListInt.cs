8ce00f9948810288cb52294d8dded50ff02c90cf
Patryk Bogdański <bogdanski.patryk1991@gmail.com>
2018-10-09 23:39:43 +0200

    Prosta implementacja listy

---
+++ DataStructures/Modules/DataStructures/ListInt.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructures
+{
+    public class ListInt : IEnumerable<int>
+    {
+        private int[] _values;
+
+        public int Count { get; private set; }
+        public int Capacity { get; private set; }
+
+        public ListInt()
+        {
+            Capacity = 64;
+            _values = new int[Capacity];
+            Count = 0;
+        }
+
+        public ListInt(IEnumerable<int> values)
+        {
+            if (values == null)
+            {
+                throw new ArgumentNullException(nameof(values));
+            }
+            Capacity = values.Count() * 2;
+            _values = new int[Capacity];
+            (values as int[]).CopyTo(_values, 0);
+            Count = values.Count();
+        }
+
+        public void Add(int value)
+        {
+            EnsureSize(Count + 1);
+            _values[Count] = value;
+            Count++;
+        }
+
+        public void Insert(int value, int index)
+        {
+            EnsureSize(Count + 1);
+            if (index < Count)
+            {
+                Array.Copy(_values, index, _values, index + 1, Count - index);
+            }
+            _values[index] = value;
+            Count++;
+        }
+
+        public bool Contains(int value)
+        {
+            for (int i = 0; i < Count; ++i)
+            {
+                if (_values[i] == value)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public int IndexOf(int value)
+        {
+            for (int i = 0; i < Count; ++i)
+            {
+                if (_values[i] == value)
+                {
+                    return i;
+                }
+            }
+            return -1;
+        }
+
+        public void Remove(int value)
+        {
+            int index = IndexOf(value);
+            if (index >= 0)
+            {
+                RemoveAt(index);
+            }
+        }
+
+        public void RemoveAt(int index)
+        {
+            if (index < 0 || index >= Count)
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+            Count--;
+            if (index < Count)
+            {
+                Array.Copy(_values, index + 1, _values, index, Count - index);
+            }
+            _values[Count] = 0;
+        }
+
+        public int this[int index]
+        {
+            get
+            {
+                if (index < 0 || index >= Capacity)
+                {
+                    throw new ArgumentOutOfRangeException();
+                }
+                return _values[index];
+            }
+            set
+            {
+                if (index < 0 || index >= Capacity)
+                {
+                    throw new ArgumentOutOfRangeException();
+                }
+                _values[index] = value;
+            }
+        }
+
+        public IEnumerator<int> GetEnumerator()
+        {
+            return (_values as IEnumerable<int>).GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return _values.GetEnumerator();
+        }
+
+        private void EnsureSize(int size)
+        {
+            if (Capacity < size)
+            {
+                Capacity *= 2;
+                int[] old = _values;
+                _values = new int[Capacity];
+                old.CopyTo(_values, 0);
+            }
+        }
+    }
+}
---
+++ DataStructures/Modules/Tests/TestListInt.cs
@@ -0,0 +1,100 @@
+﻿using DataStructures;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tests
+{
+    [TestClass]
+    public class TestListInt
+    {
+        [TestMethod]
+        public void ListInt_Ctors()
+        {
+            Assert.AreEqual(0, new ListInt().Count);
+            Assert.AreEqual(2, new ListInt(new int[] { 1, 2 }).Count);
+        }
+
+        [TestMethod]
+        public void ListInt_Index()
+        {
+            ListInt list = new ListInt(new int[] { 1, 2 });
+            Assert.AreEqual(1, list[0]);
+            Assert.AreEqual(2, list[1]);
+            list[0] = 3;
+            list[1] = 4;
+            Assert.AreEqual(3, list[0]);
+            Assert.AreEqual(4, list[1]);
+        }
+
+        [TestMethod]
+        public void ListInt_Add()
+        {
+            ListInt list = new ListInt();
+            list.Add(1);
+            list.Add(2);
+            Assert.AreEqual(2, list.Count);
+            Assert.AreEqual(1, list[0]);
+            Assert.AreEqual(2, list[1]);
+        }
+
+        [TestMethod]
+        public void ListInt_Insert()
+        {
+            ListInt list = new ListInt(new int[] { 1 });
+            list.Insert(2, 0);
+            Assert.AreEqual(2, list.Count);
+            Assert.AreEqual(2, list[0]);
+            Assert.AreEqual(1, list[1]);
+        }
+
+        [TestMethod]
+        public void ListInt_Contains()
+        {
+            ListInt list = new ListInt(new int[] { 1, 2 });
+            Assert.IsTrue(list.Contains(1));
+            Assert.IsTrue(list.Contains(2));
+            Assert.IsFalse(list.Contains(3));
+        }
+
+        [TestMethod]
+        public void ListInt_IndexOf()
+        {
+            ListInt list = new ListInt(new int[] { 1, 2 });
+            Assert.AreEqual(0, list.IndexOf(1));
+            Assert.AreEqual(1, list.IndexOf(2));
+        }
+
+        [TestMethod]
+        public void ListInt_Remove()
+        {
+            ListInt list = new ListInt(new int[] { 1, 2 });
+            list.Remove(1);
+            list.Remove(2);
+            Assert.AreEqual(0, list.Count);
+        }
+
+        [TestMethod]
+        public void ListInt_RemoveAt()
+        {
+            ListInt list = new ListInt(new int[] { 1, 2 });
+            list.RemoveAt(1);
+            Assert.AreEqual(1, list.Count);
+            Assert.AreEqual(1, list[0]);
+        }
+
+        [TestMethod]
+        public void ListInt_Resize()
+        {
+            ListInt list = new ListInt();
+            for (int i = 0; i < 1024; i++)
+            {
+                list.Add(i);
+            }
+            Assert.AreEqual(1024, list.Count);
+        }
+    }
+}
